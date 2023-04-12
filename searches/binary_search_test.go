4a86ace29bcd48161dc612b2bad9373c2fbe3ea6
pchchv <ipchchv@gmail.com>
2023-04-12 23:40:52 +0300

    Implement a UpperBound test

--- searches/binary_search_test.go
+++ searches/binary_search_test.go
@@ -37,3 +37,15 @@ func TestLowerBound(t *testing.T) {
 		}
 	}
 }
+
+func TestUpperBound(t *testing.T) {
+	for _, test := range upperBoundTests {
+		actualValue, actualError := UpperBound(test.data, test.key)
+		if actualValue != test.expected {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actualValue)
+		}
+		if actualError != test.expectedError {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected error '%s', get error '%s'", test.name, test.data, test.key, test.expectedError, actualError)
+		}
+	}
+}
--- searches/testcases.go
+++ searches/testcases.go
@@ -39,3 +39,16 @@ var lowerBoundTests = []searchTest{
 
 	{[]int{}, 2, -1, ErrNotFound, "Empty"},
 }
+
+var upperBoundTests = []searchTest{
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, -25, 0, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 1, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 5, nil, "Sanity"},
+	{[]int{1, 2, 2, 2, 2, 6, 7, 8, 9, 10}, 2, 5, nil, "Sanity"},
+
+	{[]int{2, 2, 2, 2, 2, 2, 2, 2, 2, 2}, 2, -1, ErrNotFound, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, -1, ErrNotFound, "Sanity"},
+	{[]int{1, 4, 5, 6, 7, 10}, 25, -1, ErrNotFound, "Absent"},
+
+	{[]int{}, 2, -1, ErrNotFound, "Empty"},
+}
