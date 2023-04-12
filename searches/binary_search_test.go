81fb444b9727f206d5046da39d14bd5102084ea2
pchchv <ipchchv@gmail.com>
2023-04-12 23:37:52 +0300

    Add a Binary and BinaryIterative func's tests

---
+++ searches/binary_search_test.go
@@ -0,0 +1,27 @@
+package search
+
+import "testing"
+
+func TestBinary(t *testing.T) {
+	for _, test := range searchTests {
+		actualValue, actualError := Binary(test.data, test.key, 0, len(test.data)-1)
+		if actualValue != test.expected {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actualValue)
+		}
+		if actualError != test.expectedError {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected error '%s', get error '%s'", test.name, test.data, test.key, test.expectedError, actualError)
+		}
+	}
+}
+
+func TestBinaryIterative(t *testing.T) {
+	for _, test := range searchTests {
+		actualValue, actualError := BinaryIterative(test.data, test.key)
+		if actualValue != test.expected {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actualValue)
+		}
+		if actualError != test.expectedError {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected error '%s', get error '%s'", test.name, test.data, test.key, test.expectedError, actualError)
+		}
+	}
+}
