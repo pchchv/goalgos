97a5bdb2b1e1f6ee0032f6ff301d338aa809d72d
pchchv <ipchchv@gmail.com>
2023-04-12 23:39:36 +0300

    Implement a LowerBound test

--- searches/binary_search_test.go
+++ searches/binary_search_test.go
@@ -25,3 +25,15 @@ func TestBinaryIterative(t *testing.T) {
 		}
 	}
 }
+
+func TestLowerBound(t *testing.T) {
+	for _, test := range lowerBoundTests {
+		actualValue, actualError := LowerBound(test.data, test.key)
+		if actualValue != test.expected {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected '%d', get '%d'", test.name, test.data, test.key, test.expected, actualValue)
+		}
+		if actualError != test.expectedError {
+			t.Errorf("test '%s' failed: input array '%v' with key '%d', expected error '%s', get error '%s'", test.name, test.data, test.key, test.expectedError, actualError)
+		}
+	}
+}
--- searches/testcases.go
+++ searches/testcases.go
@@ -26,3 +26,16 @@ var searchTests = []searchTest{
 
 	{[]int{}, 2, -1, ErrNotFound, "Empty"},
 }
+
+var lowerBoundTests = []searchTest{
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, -25, 0, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 1, 0, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 4, nil, "Sanity"},
+	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 9, nil, "Sanity"},
+	{[]int{1, 2, 2, 2, 2, 6, 7, 8, 9, 10}, 2, 1, nil, "Sanity"},
+	{[]int{2, 2, 2, 2, 2, 2, 2, 2, 2, 2}, 2, 0, nil, "Sanity"},
+
+	{[]int{1, 4, 5, 6, 7, 10}, 25, -1, ErrNotFound, "Absent"},
+
+	{[]int{}, 2, -1, ErrNotFound, "Empty"},
+}
