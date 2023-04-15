0487cf38498fa373d08e689a5a3924e65dbf0b13
pchchv <ipchchv@gmail.com>
2023-04-15 12:05:11 +0300

    Implement a LCM and its tests

---
+++ math/lcm/lcm.go
@@ -0,0 +1,13 @@
+package lcm
+
+import (
+	"math"
+
+	"github.com/pchchv/goalgos/math/gcd"
+)
+
+// Lcm returns the lcm of two numbers using the fact
+// that lcm(a,b) * gcd(a,b) = | a * b |
+func Lcm(a, b int64) int64 {
+	return int64(math.Abs(float64(a*b)) / float64(gcd.Iterative(a, b)))
+}
