0f4e3cbf37bb05ffbc1a00f3069487fe04fc1795
pchchv <ipchchv@gmail.com>
2023-04-17 09:49:13 +0300

    Add a tests of a stack linked-list with a stdlib

--- structure/stack/stack_test.go
+++ structure/stack/stack_test.go
@@ -1,6 +1,7 @@
 package stack
 
 import (
+	"container/list"
 	"reflect"
 	"testing"
 )
@@ -105,6 +106,65 @@ func TestStackLinkedList(t *testing.T) {
 	t.Run("Stack Peak", func(t *testing.T) {
 		if newStack.peak() != 99 {
 			t.Error("Stack Peak should return 99 but got ", newStack.peak())
+		}
+	})
+}
+
+// TestStackLinkedListWithList for testing Stack with Container/List Library (STL)
+func TestStackLinkedListWithList(t *testing.T) {
+	stackList := &SList{
+		stack: list.New(),
+	}
+
+	t.Run("Stack Push", func(t *testing.T) {
+
+		stackList.Push(2)
+		stackList.Push(3)
+
+		if stackList.Length() != 2 {
+			t.Errorf("Stack Push is not work we expected %v but got %v", 2, stackList.Length())
+		}
+	})
+
+	t.Run("Stack Pop", func(t *testing.T) {
+		pop, _ := stackList.Pop()
+
+		if stackList.Length() == 1 && pop != 3 {
+			t.Errorf("Stack Pop is not work we expected %v but got %v", 3, pop)
+		}
+	})
+
+	t.Run("Stack Peak", func(t *testing.T) {
+
+		stackList.Push(2)
+		stackList.Push(83)
+		peak, _ := stackList.Peak()
+		if peak != 83 {
+			t.Errorf("Stack Peak is not work we expected %v but got %v", 83, peak)
+		}
+	})
+
+	t.Run("Stack Length", func(t *testing.T) {
+		if stackList.Length() != 3 {
+			t.Errorf("Stack Length is not work we expected %v but got %v", 3, stackList.Length())
+		}
+	})
+
+	t.Run("Stack Empty", func(t *testing.T) {
+		if stackList.Empty() == true {
+			t.Errorf("Stack Empty is not work we expected %v but got %v", false, stackList.Empty())
+		}
+
+		d1, err := stackList.Pop()
+		d2, _ := stackList.Pop()
+		d3, _ := stackList.Pop()
+
+		if err != nil {
+			t.Errorf("got an unexpected error %v, pop1: %v, pop2: %v, pop3: %v", err, d1, d2, d3)
+		}
+
+		if stackList.Empty() == false {
+			t.Errorf("Stack Empty is not work we expected %v but got %v", true, stackList.Empty())
 		}
 	})
 }
