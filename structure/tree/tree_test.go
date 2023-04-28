0dc717fd132ff2d97c15818d9ef5e9266d4db363
pchchv <ipchchv@gmail.com>
2023-04-29 00:32:18 +0300

    Add AVL tree benchmarks

---
+++ structure/tree/tree_test.go
@@ -0,0 +1,58 @@
+package tree_test
+
+import (
+	"testing"
+
+	bt "github.com/pchchv/goalgos/structure/tree"
+)
+
+// Benchmark the comparisons between BST, AVL and RB Tree
+const testNum = 10_000
+
+func BenchmarkAVLTree_Insert(b *testing.B) {
+	helper := func() {
+		tree := bt.NewAVL[int]()
+		for i := 1; i <= testNum; i++ {
+			tree.Push(i)
+		}
+	}
+
+	for i := 0; i < b.N; i++ {
+		helper()
+	}
+
+}
+
+func BenchmarkAVLTree_Has(b *testing.B) {
+	helper := func() {
+		tree := bt.NewAVL[int]()
+		for i := 1; i <= testNum; i++ {
+			tree.Push(i)
+		}
+
+		for i := 1; i <= testNum; i++ {
+			tree.Has(i)
+		}
+	}
+
+	for i := 0; i < b.N; i++ {
+		helper()
+	}
+}
+
+func BenchmarkAVLTree_Delete(b *testing.B) {
+	helper := func() {
+		tree := bt.NewAVL[int]()
+		for i := 1; i <= testNum; i++ {
+			tree.Push(i)
+		}
+
+		for i := 1; i <= testNum; i++ {
+			tree.Delete(i)
+		}
+	}
+
+	for i := 0; i < b.N; i++ {
+		helper()
+	}
+}
