b735cab083a9d5271fca90a1e8371de93ab4859b
pchchv <ipchchv@gmail.com>
2023-04-30 00:11:33 +0300

    Add BinarySearch test

---
+++ structure/tree/example_test.go
@@ -0,0 +1,74 @@
+package tree_test
+
+import (
+	"fmt"
+	"testing"
+
+	bt "github.com/pchchv/goalgos/structure/tree"
+	"golang.org/x/exp/constraints"
+)
+
+type TestTree[T constraints.Ordered] interface {
+	Push(...T)
+	Delete(T) bool
+	Get(T) (bt.Node[T], bool)
+	Empty() bool
+	Has(T) bool
+	Depth() int
+	Max() (T, bool)
+	Min() (T, bool)
+	Predecessor(T) (T, bool)
+	Successor(T) (T, bool)
+	PreOrder() []T
+	InOrder() []T
+	PostOrder() []T
+	LevelOrder() []T
+	AccessNodesByLayer() [][]T
+}
+
+var tree TestTree[int]
+
+func TestBinarySearch(t *testing.T) {
+	tree = bt.NewBinarySearch[int]()
+
+	if tree.Empty() {
+		t.Log("Binary Search Tree is empty now.")
+	}
+
+	tree.Push(1, 4, 10)
+	tree.Push(-8)
+	nums := []int{87, 18, 10, -34}
+	tree.Push(nums...)
+	tree.Push(4) // duplicate key, dismiss it
+
+	if tree.Has(4) {
+		t.Logf("There is a node of 4")
+	}
+
+	if n, ok := tree.Get(10); ok {
+		t.Logf("node of 10: %T", n)
+	}
+
+	if ret, ok := tree.Min(); ok {
+		t.Logf("tree.Min() = %v", ret)
+	}
+
+	if ret, ok := tree.Max(); ok {
+		t.Logf("tree.Max() = %v", ret)
+	}
+
+	if ret, ok := tree.Predecessor(1); ok {
+		t.Logf("tree.Preducessor(1) = %v", ret)
+	}
+
+	if ret, ok := tree.Successor(18); ok {
+		t.Logf("tree.Successor(18) = %v", ret)
+	}
+
+	fmt.Println(tree.InOrder())
+	fmt.Println(tree.AccessNodesByLayer())
+
+	tree.Delete(18)
+	fmt.Println("Delete 18")
+	fmt.Println(tree.InOrder())
+}
