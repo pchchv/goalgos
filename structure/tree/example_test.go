419c67340d3b46b47428fc7ca3951aff4c27b949
pchchv <ipchchv@gmail.com>
2023-04-30 00:17:26 +0300

    Add a Red-Black tree tests

--- structure/tree/example_test.go
+++ structure/tree/example_test.go
@@ -30,6 +30,7 @@ var (
 	// BinarySearch, AVL and RB have completed the `TestTree` interface.
 	_ TestTree[int] = (*bt.BinarySearch[int])(nil)
 	_ TestTree[int] = (*bt.AVL[int])(nil)
+	_ TestTree[int] = (*bt.RB[int])(nil)
 
 	tree TestTree[int]
 )
@@ -122,4 +123,45 @@ func TestAVL(t *testing.T) {
 	tree.Delete(18)
 	fmt.Println("Delete 18")
 	fmt.Println(tree.InOrder())
+}
+
+func TestRB(t *testing.T) {
+	tree = bt.NewRB[int]()
+
+	if tree.Empty() {
+		t.Log("RB Tree is empty now.")
+	}
+
+	tree.Push(1, 4, 10)
+	tree.Push(-8)
+	nums := []int{87, 18, 10, -34}
+	tree.Push(nums...)
+	tree.Push(4) // duplicate key, dismiss it
+
+	if tree.Has(4) {
+		t.Logf("There is a node of 4")
+	}
+
+	if n, ok := tree.Get(10); ok {
+		t.Logf("node of 10: %T", n)
+	}
+
+	if ret, ok := tree.Min(); ok {
+		t.Logf("tree.Min() = %v", ret)
+	}
+
+	if ret, ok := tree.Max(); ok {
+		t.Logf("tree.Max() = %v", ret)
+	}
+
+	if ret, ok := tree.Predecessor(1); ok {
+		t.Logf("tree.Preducessor(1) = %v", ret)
+	}
+
+	if ret, ok := tree.Successor(18); ok {
+		t.Logf("tree.Successor(18) = %v", ret)
+	}
+
+	fmt.Println(tree.InOrder())
+	fmt.Println(tree.AccessNodesByLayer())
 }
