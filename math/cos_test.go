6042361b578c908e5579806e0e551a153ce906c1
pchchv <ipchchv@gmail.com>
2023-05-09 15:47:08 +0300

    Add a cosine tests

---
+++ math/cos_test.go
@@ -0,0 +1,47 @@
+package math_test
+
+import (
+	"math"
+	"testing"
+
+	math2 "github.com/pchchv/goalgos/math"
+)
+
+const epsilon = 0.001
+
+func TestCos(t *testing.T) {
+	tests := []struct {
+		name string
+		n    float64
+		want float64
+	}{
+		{"cos(0)", 0, 1},
+		{"cos(90)", 90, -0.447},
+		{"cos(180)", 180, -0.598},
+		{"cos(1)", 1, 0.540},
+		{"cos(π)", math.Pi, -1},
+		{"cos(π/2)", math.Pi / 2, 0},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := math2.Cos(test.n)
+			if math.Abs(got-test.want) >= epsilon {
+				t.Errorf("Cos() = %v, want %v", got, test.want)
+				t.Errorf("MATH Cos() = %v", math.Cos(test.n))
+			}
+		})
+	}
+}
+
+func BenchmarkCos(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		math2.Cos(180)
+	}
+}
+
+// BenchmarkMathCos is slower because the standard library `math.Cos` calculates a more accurate value.
+func BenchmarkMathCos(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		math.Cos(180)
+	}
+}
